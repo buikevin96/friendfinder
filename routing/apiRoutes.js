75c7e04b591d43ab382bd58689fe4fb4693376dc
buikevin96@gmail.com <buikevin96@gmail.com>
2017-08-27 23:32:08 -0400

    Updated htmlRoutes.js

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,28 +5,6 @@ app.get("/api/friends", function(req, res) {
 
 //Update information in here
 
-  // If the main route is hit, then we initiate a SQL query to grab all records.
-  // All of the resulting records are stored in the variable "result."
-  connection.query("SELECT * FROM actors where attitude = ?", [req.params.att], function(err, result) {
 
-    // We then begin building out HTML elements for the page.
-    var html = "<h1> Actors with an attitude of " + req.params.att + "</h1>";
-
-    // Here we begin an unordered list.
-    html += "<ul>";
-
-    // We then use the retrieved records from the database to populate our HTML file.
-    for (var i = 0; i < result.length; i++) {
-      html += "<li><p> ID: " + result[i].id + "</p>";
-      html += "<p>Name: " + result[i].name + " </p></li>";
-      html += "<p>Coolness Points: " + result[i].coolness_points + "<p>";
-      html += "<p>Attitude: " + result[i].attitude + "<p>";
-    }
-
-    // We close our unordered list.
-    html += "</ul>";
-
-    // Finally we send the user the HTML file we dynamically created.
-    res.send(html);
-  });
 });
+
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,32 +1,21 @@
-// A default, catch-all route that leads to home.html which displays the home page.
+// Inclue path package to get the correct file path for our html
+var path = require("path");
 
-// A GET Route to /survey which should display the survey page.
-app.get("/survey", function(req, res) {
+module.exports = function(app) {
 
-//Update information in here
-
-  // If the main route is hit, then we initiate a SQL query to grab all records.
-  // All of the resulting records are stored in the variable "result."
-  connection.query("SELECT * FROM actors where attitude = ?", [req.params.att], function(err, result) {
-
-    // We then begin building out HTML elements for the page.
-    var html = "<h1> Actors with an attitude of " + req.params.att + "</h1>";
-
-    // Here we begin an unordered list.
-    html += "<ul>";
-
-    // We then use the retrieved records from the database to populate our HTML file.
-    for (var i = 0; i < result.length; i++) {
-      html += "<li><p> ID: " + result[i].id + "</p>";
-      html += "<p>Name: " + result[i].name + " </p></li>";
-      html += "<p>Coolness Points: " + result[i].coolness_points + "<p>";
-      html += "<p>Attitude: " + result[i].attitude + "<p>";
-    }
-
-    // We close our unordered list.
-    html += "</ul>";
-
-    // Finally we send the user the HTML file we dynamically created.
-    res.send(html);
+  // A GET Route to /survey which should display the survey page.
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
-});
+
+  // A default, catch-all route that leads to home.html which displays the home page.
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  // A default, catch-all route that leads to home.html which displays the home page.
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
+
